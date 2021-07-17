88ad5f4cea47f841cc42ce2739e973be063cf6ec
Felipe-M-dev <39914824+Felipe-M-dev@users.noreply.github.com>
2021-07-17 04:00:56 -0400

    Update arreglo_notas.rb

--- arreglo_notas.rb
+++ arreglo_notas.rb
@@ -1,12 +1,15 @@
 notas = [5, 7, 1, 3, 5, 8, 9, 'N.A', 'N.A', 3]
 
-array = []
-notas.each do |notas|
-    if (notas == 'N.A')
-        array.push 2
-    else
-        array.push notas
+def promedio(notas)
+    array = []
+    notas.each do |notas|
+        if (notas == 'N.A')
+            array.push 2
+        else
+            array.push notas
+        end
     end
+    puts array.inject(0){ |sum, x| sum + x } / notas.length 
 end
 
-puts array.inject(0){ |sum, x| sum + x } / notas.length
+promedio(notas)
